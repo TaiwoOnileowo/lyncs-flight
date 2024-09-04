4dd9582ae625aefa4a5c0e404d989a5d4eb2bb4a
Taiwo Onileowo <taiwoonileowo17@gmail.com>
2024-09-05 00:42:33 +0100

    add hero text

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,12 +3,13 @@ import HeroBackground from "./HeroBackground";
 
 const Hero = () => {
   return (
-    <div className="relative p-24 w-full h-screen flex items-center overflow-hidden font-karla bg-[#7852ef]">
-      <div className="z-[100] max-w-2xl">
-        <h1 className="text-white/80  text-7xl">
+    <div className="relative p-24 w-full h-screen  flex overflow-hidden font-karla bg-[#7852ef]">
+      <div className="z-[100] max-w-2xl mt-16">
+        <h1 className="text-white/80  text-6xl font-bold">
           Book <span className="font-bold text-white">Flights</span> with{" "}
           <span className="font-bold text-white">Elegant Fashion</span>
         </h1>
+        <p className="text-white mt-6 text-xl">Lyncs flight widget appears here</p>
       </div>
       <HeroBackground />
     </div>
