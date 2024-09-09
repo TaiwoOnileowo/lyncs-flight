40d445104cd28bbcef395ee24f55dd0a1db68b38
Taiwo Onileowo <taiwoonileowo17@gmail.com>
2024-09-09 12:28:38 +0100

    minor adjustment

--- components/Hero2.tsx
+++ components/Hero2.tsx
@@ -1,4 +1,4 @@
-"use client";
+
 import React from "react";
 import Nav from "./Nav";
 import VideoRecord from "./VideoRecord";
--- components/VideoRecord.tsx
+++ components/VideoRecord.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const VideoRecord = () => {
   return (
     <div className=" relative z-[1] h-full max-xl:mt-12 rounded-full overflow-hidden w-fit md:border-[5px] border-gray-200">
-      <div className="absolute bottom-0 max-xl:-bottom-24 max-xl:-right-16 max-lg:-bottom-40 h-fit max-lg:-right-32 -right-4 z-[3] rounded-xl">
+      <div className="absolute bottom-0 max-xl:-bottom-24 max-xl:-right-16 max-lg:-bottom-32 h-fit max-lg:-right-24 -right-4 z-[3] rounded-xl">
         <video autoPlay loop muted className="w-[300px] h-fit object-cover rounded-xl ">
           <source src="/record.mp4" type="video/mp4" />
         </video>
